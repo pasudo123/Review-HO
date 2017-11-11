eac110c11b502441fb9567b6c6f00e2a0e55a8a4
pasudo123 <pasudo222@gmail.com>
2017-11-11 23:30:12 +0900

    Updates Pagination

--- pages/static/js/publicScript.js
+++ pages/static/js/publicScript.js
@@ -8,19 +8,30 @@ function findStory(n){
         data:{'number':number},
         dataType:'json',
         success:function(args){
-            var len = Object.keys(args).length;
-            // alert(len);
+            
+            $('table.reviewTable').find('tr').remove();
+            $('div.pagination').find('ul').remove();
 
-            tc = 12;
-            // var tc = len / 5;
+            var len = Object.keys(args).length;
+
+            pageSize = len / 5;
+            // 나머지 확인
             if(len % 5 != 0)
-                tc = tc + 1;
+                pageSize += 1; 
+
+            var textArr = new Array();
+            var sentArr = new Array();
 
             // 댓글 모음
             for(var i = 1; i <= len; i++){
                 var t = args[i]['text'];
                 var s = args[i]['senti'];
+                
+                textArr[i] = t;
+                sentArr[i] = s;
 
+                // +++++++++++++++처음 다섯개만
+                if(i <= 5){
                 // 해당 영화 리뷰 목록 접근
                 var list = $('div#rNp_'+ number).find('table#myTable_' + number);
                 var ele = '<tr><td><div class="reviewList_ele">';
@@ -43,6 +54,9 @@ function findStory(n){
                 
                 list.append(ele);
 
+                $('table.reviewTable').find('td').css('padding-top', '0.7%');
+                $('table.reviewTable').find('td').css('padding-bottom', '0.7%');
+
                 $('table.reviewTable').find('div.reviewList_ele').css('font-family', 'NanumSquareR');
                 $('table.reviewTable').find('div.reviewList_ele').css('font-size', '1.7vw');
                 $('table.reviewTable').find('div.reviewList_ele').css('color', 'rgb(221, 221, 221)');
@@ -50,16 +64,73 @@ function findStory(n){
 
                 $('table.reviewTable').find('img').css('width', '3%');
                 $('table.reviewTable').find('img').css('height', 'auto');
-                $('table.reviewTable').find('img').css('padding-bottom', '0.4%');
-                $('table.reviewTable').find('img').css('height', 'auto');
 
                 $('table.reviewTable').find('span').css('padding-left', '2%');
                 $('table.reviewTable').find('span').css('cursor', 'auto');
-
-                // 다섯개씩 끊어서 목록 갱신
-                if(i % 5 == 0)
-                    break;
+                }
             }
+
+            $('#pagination_'+number).materializePagination({
+                align: 'center',
+                lastPage: pageSize,
+                firstPage: 1,
+                useUrlParameter: false,
+                onClickCallback: function(requestedPage) {
+                    // 해당 페이지 넘버 반환
+                    // alert(requestedPage);
+                    startP = (requestedPage - 1) * 5 + 1;
+                    endP = requestedPage * 5;
+
+                    $('table.reviewTable').find('tr').remove();
+
+                    // 해당 페이지에 맞는 리뷰반환
+                    for(var i = startP; i <= endP; i++){
+                        var t = textArr[i];
+                        var s = sentArr[i];
+
+                        // 해당 영화 리뷰 목록 접근
+                        var list = $('div#rNp_'+ number).find('table#myTable_' + number);
+                        var ele = '<tr><td><div class="reviewList_ele">';
+
+                        // 부정 리뷰
+                        if(s == 0){
+                            ele += '<img class="reviewSenti" src="../static/Image/negIcon.png"></img>';
+                        }
+                        // 긍정 리뷰
+                        else{
+                            ele += '<img class="reviewSenti" src="../static/Image/posIcon.png"></img>';
+                        }
+
+                        skipText = t;
+                        
+                        if(t.length >= 70)
+                            skipText = t.substring(0, 70) + " ...";
+
+                        ele += '<span class="reviewText">' + skipText + '</span></div></td></tr>';
+                        
+                        list.append(ele);
+
+                        $('table.reviewTable').find('td').css('padding-top', '0.7%');
+                        $('table.reviewTable').find('td').css('padding-bottom', '0.7%');
+
+                        $('table.reviewTable').find('div.reviewList_ele').css('font-family', 'NanumSquareR');
+                        $('table.reviewTable').find('div.reviewList_ele').css('font-size', '1.7vw');
+                        $('table.reviewTable').find('div.reviewList_ele').css('color', 'rgb(221, 221, 221)');
+                        $('table.reviewTable').find('div.reviewList_ele').css('vertical-align', 'middle');
+
+                        $('table.reviewTable').find('img').css('width', '3%');
+                        $('table.reviewTable').find('img').css('height', 'auto');
+
+                        $('table.reviewTable').find('span').css('padding-left', '2%');
+                        $('table.reviewTable').find('span').css('cursor', 'auto');
+                    }
+
+                    console.log('Requested page from #pagination : ' + requestedPage);
+                }
+            });
+
+            // 부트스트랩 css 조정
+            $('#pagination_' + number).find('ul').css('margin', '0');
         }
     });
 }
@@ -79,3 +150,4 @@ function findSpelcial(){
 function pageNavigator(){
     
 }
+
