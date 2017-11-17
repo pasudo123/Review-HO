18e981f7001fd8ae9876d6f43fd9abd959c3a8fc
pasudo123 <pasudo222@gmail.com>
2017-11-17 17:11:34 +0900

    Updates url.py

--- pages/urls.py
+++ pages/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^MainPage$', views.show_main, name='show_main'),
-    url(r'^Intro$', views.show_intro, name='show_intro'),
     url(r'^Demo$', views.show_demo, name='show_demo'),
     url(r'^Demo/analyze/$', views.getAnalyze_senti, name='getAnalyze_senti'),
     url(r'^MovieGuide$', views.show_movie, name='show_movie'),
